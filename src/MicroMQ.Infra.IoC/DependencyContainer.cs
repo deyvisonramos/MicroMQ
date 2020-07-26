f4da3a197c8d3585ccb92bd03300d52b9cdcc78b
Deyvison Ramos <deyvisonr0@gmail.com>
2020-07-26 17:39:47 -0300

    feat: adds the IoC project to the solution

---
+++ src/MicroMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,15 @@
+﻿using MicroMQ.Domain.Core.Bus;
+using MicroMQ.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MicroMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+} 
