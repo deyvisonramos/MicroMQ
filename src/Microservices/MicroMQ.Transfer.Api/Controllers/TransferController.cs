40b30c8e967784a25948f8b9132fcc6e4677bde0
Deyvison Ramos <deyvisonr0@gmail.com>
2020-07-28 23:33:49 -0300

    feat: initial commit for transfer microservices

---
+++ src/Microservices/MicroMQ.Transfer.Api/Controllers/TransferController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroMQ.Transfer.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransferController : ControllerBase
+    {
+        private readonly ILogger<TransferController> _logger;
+
+        public TransferController(ILogger<TransferController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new int[] { });
+        }
+    }
+}
